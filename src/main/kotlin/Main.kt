75129a4487b2e413c770438b0abe0297e48f5db7
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-08-19 14:52:50 +0100

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>){
+    var x = 1
+    println("Before the loop.git config core.autocrlf false x = $x.")
+    while (x < 4) {
+        println("In the loop. x = $x.")
+        x += 1
+    }
+    println("After the loop. x = $x.")
+}
