30ed307cfbae39348721e2ae7597f8ad4add8ae2
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-08-22 16:35:29 +0100

    RPS game is working

--- RockPaperScissors/src/main/kotlin/Game.kt
+++ RockPaperScissors/src/main/kotlin/Game.kt
@@ -2,6 +2,7 @@ fun main(args: Array<String>) {
     val options = arrayOf("Rock", "Paper", "Scissors")
     val gameChoice = getGameChoice(options)
     val userChoice = getUserChoice(options)
+    printResult(userChoice, gameChoice)
 }
 
 fun getGameChoice(optionsParam: Array<String>): String {
@@ -27,4 +28,14 @@ fun getUserChoice(optionsParam: Array<String>): String {
         if (!isValidChoice) println("You must enter a valid choice.")
     }
     return userChoice
+}
+
+fun printResult(userChoice: String, gameChoice: String) {
+    val result: String
+    if (userChoice == gameChoice) result = "Tie!"
+    else if ((userChoice == "Rock" && gameChoice == "Scissors") ||
+            (userChoice == "Paper" && gameChoice == "Rock") ||
+            (userChoice == "Scissors" && gameChoice == "Paper")) result = "You win!"
+    else result = "You lose!"
+    println("You chose $userChoice. I chose $gameChoice. $result")
 }
