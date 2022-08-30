504854404d69210c9abd2dfe96e21a9ecf8ddcc8
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-08-30 07:52:43 +0100

    adds basic animal class

---
+++ Animals/src/main/kotlin/Animal.kt
@@ -0,0 +1,22 @@
+class Animaks {
+    val image = ""
+    val food = ""
+    val habitat = ""
+    var hunger = 10
+
+    fun makeNoise() {
+        println("The animal is making a noise")
+    }
+
+    fun eat() {
+        println("The animal is eating")
+    }
+
+    fun roam() {
+        println("The animal is roaming")
+    }
+
+    fun sleep() {
+        println("The animal is sleeping")
+    }
+}
