372506403666c9b40a5a65dfd7e35199b45d5c5b
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-09-01 17:30:45 +0100

    passes first 3 tests after refactor

--- FizzBuzz/src/main/kotlin/Main.kt
+++ FizzBuzz/src/main/kotlin/Main.kt
@@ -1,12 +1,20 @@
-fun main(args: Array<String>) {
+fun main() {
 }
 
-fun fizzBuzz(numbers: Array<Int>) : String {
- return if (numbers contentEquals arrayOf(1)) {
-  "1"
- } else if (numbers contentEquals arrayOf(1, 2)) {
-  "1\n2"
- } else {
-  "1\n2\nFizz"
- }
+fun fizzBuzz(numbers: Array<Int>) :String {
+ return numbers.map{
+  if (it == 3) {
+   "Fizz"
+  } else {
+   it
+  }
+ }.joinToString("\n")
+
+// return if (numbers contentEquals arrayOf(1)) {
+//  "1"
+// } else if (numbers contentEquals arrayOf(1, 2)) {
+//  "1\n2"
+// } else {
+//  "1\n2\nFizz"
+// }
 }
